b77b679fcc459703f9c577d8a90e4044b51d0ead
antcoop <acooper@hireology.com>
2021-02-24 13:08:57 -0600

    updated cross origin

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const options = {
   method: 'GET',
   headers: {
+    'Access-Control-Allow-Origin': '*',
     'credentials': 'include',
     'app_id': '4f674bbe',
     'app_key': '874c4dbbaaf0f8e139c64ee1ee2ad27c',
